a76a9d2d54fd6427c34f1f50ebb5ab10e33262bc
Hasan <wilayudhah@gmail.com>
2026-05-16 05:19:43 +0700

    FIXllinksuser1

---
+++ app/api/links/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+
+import { supabase } from "@/lib/supabase";
+
+export async function GET(
+  req: Request
+) {
+
+  const { searchParams } =
+    new URL(req.url);
+
+  const user_id =
+    searchParams.get("user_id");
+
+  let query =
+    supabase
+      .from("links")
+      .select("*")
+      .order("id", {
+        ascending: false
+      });
+
+  if (user_id) {
+
+    query =
+      query.eq(
+        "user_id",
+        user_id
+      );
+  }
+
+  const { data, error } =
+    await query;
+
+  if (error) {
+
+    return NextResponse.json(
+      {
+        error: error.message
+      },
+      {
+        status: 500
+      }
+    );
+  }
+
+  return NextResponse.json(data);
+}
+

671f4bc4561739a453c33b86d627471ccf6075ff
Hasan <wilayudhah@gmail.com>
2026-05-15 09:40:15 +0700

    fix supabase env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-
+const supabaseUrl =
   process.env
-    .NEXT_PUBLIC_SUPABASE_URL!,
+    .NEXT_PUBLIC_SUPABASE_URL || "";
 
+const supabaseKey =
   process.env
-    .NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    .NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
-);
-
+export const supabase =
+  createClient(
+    supabaseUrl,
+    supabaseKey
+  );
